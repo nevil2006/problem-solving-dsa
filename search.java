e9c3803a7c713180017477580a6c678399da4077
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-09-17 19:24:41 +0530

    A

--- search.java
+++ search.java
@@ -1,3 +1,20 @@
+import java.util.*;
+
 public class search {
-    
+    public static void main(String[] args) {
+
+        int[] arr = {10, 60, 30, 40, 50};
+
+        int target = 60;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                System.out.println("Found");
+                return;
+            }
+        }
+
+        System.out.println("Not Found");
+    }
 }
