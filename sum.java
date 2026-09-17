0e64b383d8936ec2ab75bd2db2f23f5145ad5d20
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-09-17 10:18:26 +0530

    A

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args) {
+        int sum=0;
+        int[]arr={1,2,3,4,5};
+        for(int i = 0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+    
+}
