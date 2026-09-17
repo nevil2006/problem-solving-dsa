bb9baea2f9ad54671f8154c273883b00d3e4e311
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-09-17 20:22:02 +0530

    A

---
+++ Sorted.java
@@ -0,0 +1,16 @@
+public class Sorted {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            if (arr[i] > arr[i + 1]) {
+                System.out.println("Not Sorted");
+                return;
+            }
+        }
+
+        System.out.println("Sorted");
+    }
+}
